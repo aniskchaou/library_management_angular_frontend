ed136aeebf71b1b68134ce4ffd6c084be504776a
Anis KCHAOU <16862158+aniskchaou@users.noreply.github.com>
2025-02-01 17:26:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
+
+
 app.use(express.static(__dirname + '/dist/my-app-angular/'));
 
 app.get('/', function(req,res,next) {
